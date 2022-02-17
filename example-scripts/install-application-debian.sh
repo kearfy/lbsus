af873b2b32e03d756943c33d78408c6dfb491254
Micha de Vries <contact@michadevries.nl>
2022-02-17 13:45:41 +0100

    Update install-application-debian.sh

--- example-scripts/install-application-debian.sh
+++ example-scripts/install-application-debian.sh
@@ -1,12 +1,12 @@
 # Obtain superuser privileges
-echo PASS | sudo -S -v
+echo 'PASS' | sudo -S -v
 
 # Create temporary filename, define target download file.
 TEMPFILE="$(mktemp /tmp/installer.XXXXXXXXXXX)"
 DOWNLOAD="https://download.url/filename.deb"
 
 # Download the installer file
-wget -q -O $DOWNLOAD $TEMPFILE
+wget -q -O $TEMPFILE $DOWNLOAD
 
 # Install the file just downloaded
 sudo dpkg -i $TEMPFILE
